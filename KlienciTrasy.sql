8dc30727745892deaac13ed1355fbc3ff3f216fe
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2023-09-06 09:40:45 +0200

    new query

---
+++ KlienciTrasy.sql
@@ -0,0 +1,25 @@
+SELECT * FROM OPENQUERY(gonet,
+'select distinct
+k.id "ID Klient"
+,k.PELNANAZWA "Nazwa"
+,s1.NAZWA "Kraj"
+,k.KODPOCZTOWY "Kod"
+,s2.NAZWA "Miejscowoœæ"
+,k.Adres "Ulica"
+,k.ADRESDOM "Numer"
+,k.IDMANAGERA "ID PH"
+,i.DATASTART "Data Start"
+,i.DATAKONIEC "Data Koniec"
+
+from inwestycja i
+left join inwestycjaetap ie on i.id = ie.idinwestycja
+left join grupyinwestycji g on ie.id = g.idetapu
+left join kontrahent k on k.id = g.idkontrahenta
+left join slownik s1 on s1.id = k.idkraj
+left join slownik s2 on s2.id = k.idmiasto
+
+where ie.idtyp = 4292 -- Tylko kontrahenci z etapem ''Wizyta u klienta''
+and i.IDMANAGERA = 78 -- Trasy Automat jako prowadz¹cy
+'
+
+)
---
+++ PrzedstawicieleWizyty.sql
@@ -0,0 +1,29 @@
+SELECT CONVERT(date, [Data rozpoczecia]) as Data
+,[ID Wizyty]
+,[ID PH]
+,[ID Klient]
+,cast([Data rozpoczecia] as time) as [Godzina rozpoczecia]
+,cast([Data zakonczenia] as time) as[Godzina zakonczenia]
+
+FROM OPENQUERY(gonet,
+	'Select
+	Z.ID "ID Wizyty"
+	,KH.ID "ID Klient"
+	,O.ID "ID PH"
+	,Z.TERMINROZPOCZECIA "Data rozpoczecia"
+	,Z.TERMINZAKONCZENIA "Data zakonczenia"
+                                          
+	FROM KORESPONDENCJADZIENNIK KD 
+	JOIN KORESPONDENCJA KO ON KO.ID = KD.IDKORESPONDENCJI
+	JOIN ZADANIA Z On Z.IDKORESPONDENCJADZIENNIKROOT = KD.ID                                                          
+	JOIN KONTRAHENT KH On KH.ID = KD.IDKONTRAHENTA
+	LEFT JOIN SLOWNIK S1 On S1.ID = KH.IDMIASTO
+	JOIN OPERATOR O On O.ID = KO.IDOPERATORA
+
+	WHERE KD.USUNIETY = 0
+	AND KD.ARC = 0
+	AND KO.USUNIETY = 0
+	AND KO.ARC = 0
+	AND Z.TERMINROZPOCZECIA = GETDATE() - 1
+	'
+	)
