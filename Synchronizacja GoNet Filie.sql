660c156c84562a1b32291691f8a53ad3388fa02a
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2023-10-17 14:19:50 +0200

    made a procedure, change column names to snake_case

--- Synchronizacja GoNet Filie.sql
+++ Synchronizacja GoNet Filie.sql
@@ -1,9 +1,27 @@
+USE [CDNXL_GASKA]
+GO
+/****** Object:  StoredProcedure [dbo].[GoNet_Synchronizacja_Filie]    Script Date: 16.10.2023 14:38:28 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[GoNet_Synchronizacja_Filie] 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
 WITH
 Obrot AS (select KnA_GIDNumer as [Obr_KnAID]
 ,isnull(sum(case when TrN_TrNRok = YEAR(getdate()) and TrN_Data2 <= DATEDIFF(DD,'18001228', getdate()) then tre_ksiegowanetto else null end),0) as [Obr_TenRok]
 ,isnull(sum(case when TrN_TrNRok = YEAR(DATEADD(YEAR, -1, getdate())) and TrN_Data2 <= DATEDIFF(DD,'18001228', DATEADD(year, -1, getdate())) then tre_ksiegowanetto else null end),0) as [Obr_RokTemu]
 from cdn.KntAdresy with(nolock)
-join cdn.TraNag with(nolock) on KnA_GIDNumer=TrN_KnANumer AND KnA_GIDTyp=TrN_KnATyp
+join cdn.TraNag with(nolock) on KnA_GIDNumer=TrN_AdWNumer
 join cdn.traelem with(nolock) on TrN_GIDNumer=TrE_GIDNumer
 where TrN_Data2 > DATEDIFF(DD,'18001228',GETDATE()-760)
 group by KnA_GIDNumer)
@@ -21,7 +39,7 @@ group by KnA_GIDNumer)
 ,isnull(sum(case when TrN_Data2 > DATEDIFF(DD,'18001228',GETDATE()-365) and SLW_WartoscS = 'Wewnêtrzne' then tre_ksiegowanetto else null end),0) as [OKR_Wewnêtrzne]
 ,isnull(sum(case when TrN_Data2 > DATEDIFF(DD,'18001228',GETDATE()-365) and SLW_WartoscS = 'Wyposa¿. gosp. i warszt.' then tre_ksiegowanetto else null end),0) as [OKR_Gospodarstwo]
 from cdn.KntAdresy with(nolock)
-join cdn.TraNag with(nolock) on KnA_GIDNumer=TrN_KnANumer AND KnA_GIDTyp=TrN_KnATyp
+join cdn.TraNag with(nolock) on KnA_GIDNumer=TrN_AdWNumer
 join cdn.traelem with(nolock) on TrN_GIDNumer=TrE_GIDNumer
 join cdn.TwrKarty with(nolock) on Twr_GIDNumer=TrE_TwrNumer
 join cdn.Slowniki with(nolock) on SLW_ID=Twr_Notowania
@@ -31,7 +49,7 @@ group by KnA_GIDNumer)
 ,PrzeterminowePlatnosci AS (select KnA_GIDNumer as [Plat_KnAID]
 ,sum(TrP_Pozostaje) as [Plat_Suma]
 from cdn.KntAdresy with(nolock)
-join cdn.TraNag with(nolock) on KnA_GIDNumer=TrN_KnANumer AND KnA_GIDTyp=TrN_KnATyp
+join cdn.TraNag with(nolock) on KnA_GIDNumer=TrN_AdWNumer
 join cdn.TraPlat with(nolock) on TrN_GIDTyp=TrP_GIDTyp AND TrN_GIDNumer=TrP_GIDNumer
 where KnA_GIDNumer=TrN_KntNumer and TrN_KntTyp=32
 and TrN_Data2 > DATEDIFF(DD,'18001228',GETDATE()-365) 
@@ -55,52 +73,47 @@ where RLN_DataWyst >  DATEDIFF(DD,'18001228',GETDATE()-365)
 group by RLE_Status, RLN_KnANumer)
 
 select 
-Knt_GIDNumer as [Centrala ID]
-,Knt_Akronim as [Centrala Akronim]
-,KnA_GIDNumer as [Filia ID]
-,KnA_Akronim as [Filia Akronim]
-,KnA_Miasto as [Miasto]
-,KnA_Ulica as [Ulica]
-,KnA_KodP as [KodP]
-,KnA_Kraj as [Kraj]
-,KnA_EMail as [Email]
-,KnA_Telefon1 as [Telefon]
-,KnA_Nip as [NIP]
-,Prc_Imie1 + ' ' + Prc_Nazwisko as [Prowadz¹cy]
+Knt_GIDNumer as [centrala_id]
+,Knt_Akronim as [centrala_akronim]
+,KnA_GIDNumer as [filia_id]
+,KnA_Akronim as [filia_akronim]
+,KnA_Miasto as [miasto]
+,KnA_Ulica as [ulica]
+,KnA_KodP as [kodp]
+,KnA_Kraj as [kraj]
+,KnA_EMail as [email]
+,KnA_Telefon1 as [telefon]
+,KnA_Nip as [nip]
+,Prc_Imie1 as [prowadzacy_imie]
+,Prc_Nazwisko as [prowadzacy_nazwisko]
+
 --Obroty
 ,isnull(convert(varchar(25),Obr_RokTemu) + ' // ' + convert(varchar(25),Obr_TenRok) + ' (' + convert(varchar(25),CEILING((Obr_TenRok - Obr_RokTemu) / case when Obr_RokTemu = 0 then 0.01 else Obr_RokTemu end * 100)) + '%)','0.00 // 0.00 (0%)') as [Obrót Rok do Roku]
-,isnull(OKR_Akcesoria,0) as [Akcesoria i normalia]
-,isnull(OKR_Ciagniki,0) as [Czêœci do ci¹gników]
-,isnull(OKR_Kombajny,0) as [Czêœci kombajnów, sieczkarni i pras]
-,isnull(OKR_Filtry,0) as [Filtry i oleje]
-,isnull(OKR_£añcuchy,0) as [£añcuchy]
-,isnull(OKR_£o¿yska,0) as [£o¿yska]
-,isnull(OKR_Pasy,0) as [Pasy]
-,isnull(OKR_Sprzêg³a,0) as [Sprzêg³a]
-,isnull(OKR_Uprawa,0) as [Uprawa ziemi]
-,isnull(OKR_Wewnêtrzne,0) as [Wewnêtrzne]
-,isnull(OKR_Gospodarstwo,0) as [Wyposa¿. gosp. i warszt.]
-
---Reklamacje
-,isnull(STUFF((Select ', ' + Rekl_Status + ' - ' + convert(varchar(25),Rekl_Suma)
-from cdn.KntAdresy ss with(nolock) 
-left join Reklamacje on Knt_GIDNumer = Rekl_KnAID 
-where sa.KnA_GIDNumer = ss.KnA_GIDNumer  
-FOR XML PATH('')), 1, 1, ''),'') as [Reklamacje]
+,isnull(OKR_Akcesoria,0) as [akcesoria_i_normalia]
+,isnull(OKR_Ciagniki,0) as [czesci_do_ciagnikow]
+,isnull(OKR_Kombajny,0) as [czesci_kombajnow_sieczkarni_i_pras]
+,isnull(OKR_Filtry,0) as [filtry_i_oleje]
+,isnull(OKR_£añcuchy,0) as [lancuchy]
+,isnull(OKR_£o¿yska,0) as [lozyska]
+,isnull(OKR_Pasy,0) as [pasy]
+,isnull(OKR_Sprzêg³a,0) as [sprzêg³a]
+,isnull(OKR_Uprawa,0) as [uprawa_ziemi]
+,isnull(OKR_Wewnêtrzne,0) as [wewnetrzne]
+,isnull(OKR_Gospodarstwo,0) as [wyposazenie_gosp]
 
 --Przeterminowane P³atnoœci
-,isnull(Plat_Suma,0) as [Przeterminowane P³atnoœci]
+,isnull(Plat_Suma,0) as [przeterminowane_platnosci]
 
 --Atrybuty
-,rk.Atr_Wartosc as [Przedstawiciele rodzaj kontrahenta]
-,rko.Atr_Wartosc as [Przedstawiciele rodzaj kontrahenta opis]
-,zdk.Atr_Wartosc as [Przedstawiciele zakres dzia³alnoœci kontrahenta]
-,zdko.Atr_Wartosc as [Przedstawiciele zakres dzia³alnosci kontrahenta opis]
-,replace(replace(pu.Atr_Wartosc,'<',''),'>','') as [Promesa uczestnik]
-,replace(puo.Atr_Wartosc,'.',',') as [Promesa uzyskany obrot]
-,CONVERT(DATE, DATEADD(DAY,convert(int,ppo.Atr_Wartosc),'1800-12-28')) as [Promesa pocz¹tek okresu]
-,co.Atr_Wartosc as [Czy odwiedzaæ]
-,piw.Atr_Wartosc as [Potrzebna iloœæ wizyt]
+,rk.Atr_Wartosc as [przedstawiciele_rodzaj_kontrahenta]
+,rko.Atr_Wartosc as [przedstawiciele_rodzaj_kontrahenta_opis]
+,zdk.Atr_Wartosc as [przedstawiciele_zakres_dzialalnosci_kontrahenta]
+,zdko.Atr_Wartosc as [przedstawiciele_zakres_dzialalnosci_kontrahenta_opis]
+,replace(replace(pu.Atr_Wartosc,'<',''),'>','') as [promesa_uczestnik]
+,replace(puo.Atr_Wartosc,'.',',') as [promesa_uzyskany_obrot]
+,CONVERT(DATE, DATEADD(DAY,convert(int,ppo.Atr_Wartosc),'1800-12-28')) as [promesa_poczatek_okresu]
+,co.Atr_Wartosc as [czy_odwiedzac]
+,piw.Atr_Wartosc as [potrzebna_ilosc_wizyt]
 
 
 from cdn.KntAdresy sa
@@ -108,9 +121,9 @@ join cdn.KntKarty on Knt_GIDNumer=KnA_KntNumer and KnA_KntTyp=32
 join cdn.Rejony on REJ_Id=KnA_RegionCRM
 join cdn.KntOpiekun on REJ_Id=KtO_KntNumer
 join cdn.PrcKarty on Prc_GIDNumer=KtO_PrcNumer	
-left join Obrot on Knt_GIDNumer = Obr_KnAID
-left join ObrotKategorieRynkowe on Knt_GIDNumer = OKR_KnAID
-left join PrzeterminowePlatnosci on Knt_GIDNumer = Plat_KnAID
+left join Obrot on KnA_GIDNumer = Obr_KnAID
+left join ObrotKategorieRynkowe on KnA_GIDNumer = OKR_KnAID
+left join PrzeterminowePlatnosci on KnA_GIDNumer = Plat_KnAID
 left join cdn.Atrybuty rk with(nolock) on KnA_GIDTyp=rk.Atr_ObiTyp AND KnA_GIDNumer=rk.Atr_ObiNumer and (rk.Atr_OBITyp=864 or rk.Atr_OBITyp=896) and rk.atr_atkid = 453
 left join cdn.Atrybuty rko with(nolock) on KnA_GIDTyp=rko.Atr_ObiTyp AND KnA_GIDNumer=rko.Atr_ObiNumer and (rko.Atr_OBITyp=864 or rko.Atr_OBITyp=896)  and rko.atr_atkid = 454
 left join cdn.Atrybuty zdk with(nolock) on KnA_GIDTyp=zdk.Atr_ObiTyp AND KnA_GIDNumer=zdk.Atr_ObiNumer  and (zdk.Atr_OBITyp=864 or zdk.Atr_OBITyp=896) and zdk.atr_atkid = 455
@@ -123,3 +136,4 @@ left join cdn.Atrybuty piw with(nolock) on KnA_GIDTyp=piw.Atr_ObiTyp AND KnA_GID
 
 
 where KtO_Glowny = 0 and KnA_AdresBank = 1 and KnA_DataArc = ''
+END
