1c77a76c1c8d09a8aaa4302016658d50d3d2907a
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-02-27 10:05:43 +0100

    delete Bin

--- Bin/Usuwanie 2giego emaila.sql
+++
@@ -1,4 +0,0 @@
-Update cdn.KntKarty
-set knt_Email = SUBSTRING (knt_email, 1, CHARINDEX (';', knt_email) - 1)
-where Knt_Akronim = 'SAFFA'
---select knt_akronim as Akronim, Knt_EMail as mail, SUBSTRING (knt_email, 1, CHARINDEX (';', knt_email) - 1) AS skr_mail from cdn.KntKarty where Knt_Akronim = 'SAFFA'
